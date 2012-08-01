68a117578ff124b63b58fde4cc35e3b692827111
Emily Ellison <emilyellison986@gmail.com>
2012-07-31 19:48:09 -0500

    Add Model Validations to Error

--- app/models/error.rb
+++ app/models/error.rb
@@ -1,3 +1,9 @@
 class Error < ActiveRecord::Base
   attr_accessible :error_message, :explanation, :name, :title
+  
+  validates :title, presence: true
+  validates :name, presence: true
+  validates :error_message, presence: true
+  validates :explanation, presence: true
+  
 end
