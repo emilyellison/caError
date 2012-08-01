8b0fadfcd13687cf6f07034d8561870ca1a90093
Emily Ellison <emilyellison986@gmail.com>
2012-07-31 19:25:57 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaError::Application.config.secret_token = '707488d0ccd5c83acd9321eba75e114dabe9a9667d6be2dcfe34b72830476b01c7b0d933dc80cec4c1688a79e3b07424f41d796349439daf3a887dda5da16d63'
